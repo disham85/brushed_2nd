13bfc2cacb5dcfc85fc5c3105273f02f7b41cf8c
Dmitrij Baranov <amidpost1@gmail.com>
2017-08-02 14:51:02 +0300

    first commit

---
+++ js/user.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots:true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+});
